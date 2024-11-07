7803454ca08553e45ce17cbd58990ef8e2c1c62c
davidhodge931 <davidhodge931@gmail.com>
2024-11-08 10:17:46 +1300

    Update

--- R/shosha/2-scrape-a-product.R
+++ R/shosha/2-scrape-a-product.R
@@ -4,25 +4,50 @@ library(rvest)
 
 url <- "https://www.shosha.co.nz/sour-apple-salty-smart-bar-replacement-pod" #works
 url <- "https://www.shosha.co.nz/tote-aoturoa-green-grape-freebase-e-liquid-30ml" #works 
-url <- "https://www.shosha.co.nz/brands/freebase-e-liquids-brands/tote-aoturoa-e-liquids" #doesn't
+url <- "https://www.shosha.co.nz/brands/freebase-e-liquids-brands/tote-aoturoa-green" #doesn't NOT A PRODUCT
 url <- "https://www.shosha.co.nz/smok-mag-solo-kit" #works
-
-url <- urls[991]
-
-url |> 
-  read_html_live() |> 
-  html_elements(".richContent-root-CMO p")  |> 
-  html_text2() 
+url <- "https://www.shosha.co.nz/tote-aoturoa-green-grape-nicotine-salt-e-liquid-30ml"
+# url <- urls[991]
 
 #product name
-url |> 
+name <- url |> 
   read_html_live() |> 
   html_nodes("h1.productFullDetail-productName-6ZL")  |> 
-  html_text2()
+  html_text()
 
-#details list
-url |> 
+#product brand
+brand <- url |> 
+  read_html_live() |> 
+  html_nodes("div.productFullDetail-brand-zRg")  |> 
+  html_text() |> 
+  str_sub(4) |> #remove ' by'  
+  str_trim()
+
+#details list#details listinfert
+details <- url |> 
   read_html_live() |> 
   html_nodes(".richContent-root-CMO p")  |> 
-  html_text2()
+  html_text()
 
+flavour <- details |> 
+  str_subset("Flavor Profile:") |> 
+  str_remove("Flavor Profile:") |> 
+  str_trim()
+
+nicotine <- details |> 
+  str_subset("Nicotine Concentration:") |> 
+  str_remove("Nicotine Concentration:") |> 
+  str_trim()
+
+vg_pg <- details |> 
+  str_subset("VG/PG:") |> 
+  str_remove("VG/PG:") |> 
+  str_trim()
+
+tibble(
+ name = name,
+ brand = brand,
+ nicotine = nicotine,
+ flavour = flavour,
+ vg_pg = vg_pg,
+) 
