98e5211b6a25a8d8d4ef743615c6437b03048869
davidhodge931 <davidhodge931@gmail.com>
2024-12-06 14:14:48 +1300

    Update

--- shosha/R/0-run.R
+++ shosha/R/0-run.R
@@ -1,15 +1,14 @@
 # Scraping process
-# E: clean data to extract nicotine, flavour, price, vgpg, etc  
 
 # 1. Generate a unique folder name and file path
 timestamp <- format(Sys.time(), "%Y-%m-%d %H-%M-%S")  
 folder_path <- fs::path("shosha", "data", timestamp)
 fs::dir_create(folder_path) #create dir to save data into
 
-#2. save sitemap into latest data folder
+#2. mannually save sitemap into data subfolder with latest timestamp
+# https://www.shosha.co.nz/sitemap_nz.xml
 
-#3. run other scripts
-
+#3. scrape the data and then clean into an output dataset
 scripts <- fs::dir_ls(fs::path("shosha", "R")) |>
   str_subset("z-archive", negate = TRUE) |>
   str_subset("0-run", negate = TRUE) 
