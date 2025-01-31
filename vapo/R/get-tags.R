288b5d4302dc5ab708dbee3ea7e54fcb2c2e2c61
davidhodge931 <davidhodge931@gmail.com>
2025-01-31 13:06:53 +1300

    Update

---
+++ vapo/R/get-tags.R
@@ -0,0 +1,75 @@
+library(tidyverse)
+library(chromote)
+library(rvest)
+
+url <- "https://www.vapo.co.nz/products/vapo-eliquid-tobacco"
+
+url_html_live <- url |> 
+  read_html_live()
+
+# Sys.sleep(10) #otherwise category fails
+
+#name
+name <- url_html_live |> 
+  html_elements("h1.product-title")  |> 
+  html_text2()
+
+if (vctrs::vec_is_empty(name)) name <- NA
+
+name
+
+#brand
+brand <- url_html_live |> 
+  html_elements("p.product--text")  |> 
+  html_text2() |> 
+  str_sub(4) |> #remove ' by'  
+  str_trim()
+
+if (vctrs::vec_is_empty(brand)) brand <- NA
+
+brand 
+
+#category
+category <- url_html_live |>
+  html_elements("a.breadcrumbs-link-mHX.breadcrumbs-text-lAa") |> 
+  html_text2() |> 
+  magrittr::extract(2) 
+
+if (vctrs::vec_is_empty(category)) category <- NA
+
+category
+
+#price
+price <- url_html_live |>
+  html_elements("div.productFullDetail-price-p6T") |> 
+  html_text2() 
+
+if (vctrs::vec_is_empty(price)) price <- NA
+
+price
+
+#buttons
+buttons <- url_html_live |>
+  html_elements("button.tileNicotine-root-syX span") |> 
+  html_text2() |>
+  stringr::str_flatten_comma()
+
+if (vctrs::vec_is_empty(buttons)) buttons <- NA
+
+buttons
+
+#details
+details <- url_html_live |> 
+  html_elements(".richContent-root-CMO p") |> 
+  html_text2()
+
+#bind together
+tibble(
+  name = name,
+  brand = brand,
+  category = category,
+  price = price,
+  buttons = buttons,
+  details = details,
+) 
+
