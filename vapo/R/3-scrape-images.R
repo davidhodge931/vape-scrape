ad19da26e4fb9a1f7a0d2ee6cb3dac5de2c943d1
skittogh <106459447+skittogh@users.noreply.github.com>
2025-02-04 14:23:24 +1300

    Update 3-scrape-images.R
    edited to scrape images from vapo

--- vapo/R/3-scrape-images.R
+++ vapo/R/3-scrape-images.R
@@ -28,13 +28,12 @@ urls <- xml_text(nodes) |>
 
 #as products are listed as direct children of https://www.vapo.co.nz/
 #filter so that only urls with 3 slashs are included, as others do not relate to products
-
+# for vapo we wantr 4 slashes for products
 urls <- urls |>
   mutate(slash_count = str_count(url, pattern = "/")) |>
-  mutate(slash_count_3 = slash_count == 3) |>
-  filter(slash_count_3) |>
-  select(-slash_count, -slash_count_3)
-
+  mutate(slash_count_4 = slash_count == 4) |>
+  filter(slash_count_4) |>
+  select(-slash_count, -slash_count_4)
 urls <- urls |> pull()
 
 ################################################################################
@@ -60,12 +59,12 @@ wait_for_elements <- function(url_html_live, selector, timeout = 3, sleep = 2 * 
     # Try to extract the elements
     elements <- url_html_live %>%
       html_elements(selector)
-
+    
     # If the elements are found or the timeout is reached, return the result
     if (length(elements) > 0 || as.numeric(difftime(Sys.time(), start_time, units = "secs")) > timeout) {
       return(elements) #return breaks the loop
     }
-
+    
     # Otherwise, wait and we will have another crack
     Sys.sleep(sleep)
     cat("---------- Didn't get html elements for: ", selector, ". It may not exist, but having another go ----------", "\n")
@@ -82,23 +81,31 @@ wait_for_elements <- function(url_html_live, selector, timeout = 3, sleep = 2 * 
 # until elements are retrieved (since sometimes they are not retrieved
 # on the first go for some reason)
 get_html_with_retry <- function(url, retries = 3, delay = 5 * 2) {
-
+  
   attempt <- 1
   while(attempt <= retries) { # loop for number of iterations set in retries
-
+    
     try({
       # Attempt to scrape the page using read_html_live
       url_html_live <- read_html_live(url)  # Use rvest read_html_live to scrape the JS generated page
-
+      
       Sys.sleep(5)  # Polite delay as per: print(bow("https://www.vapo.co.nz/"))
-
-      image <- wait_for_elements(url_html_live, "div.carousel2-imageContainer-Xbc img") |>
+      
+      
+      #testing elements
+      #image <- url_html_live %>% html_elements("div.swiper-slide img") |>
+      # html_attr("src")
+      # image_url <- image[1]
+      
+      # image <- wait_for_elements(url_html_live, "div.carousel2-imageContainer-Xbc img") |>
+      image <- wait_for_elements(url_html_live, "div.swiper-slide img") |>
         html_attr("src")
-      image_url <- image[grepl("^https?://", image)][1] #get the first URL from the extracted html elements
-
+      # image_url <- image[grepl("^https?://", image)][1] #get the first URL from the extracted html elements
+      image_url <- paste0("https:",image[1]) #get the first URL from the extracted html elements
+      
       return(list(image_url = image_url))  #return extracted data and end the loop
     }, silent = TRUE) #end of try with error messages suppressed
-
+    
     # If we reach this point, the attempt failed - wait and try again - but if attempt > retries then stop with message
     if (attempt < retries) {
       message(paste("Attempt", attempt, "failed. Retrying in", delay, "seconds..."))
@@ -119,7 +126,7 @@ imap(urls, function(x, i) {
   tryCatch({
     # print(paste("Loop number:", i + last_saved_row)) # Adjust loop number based on last saved row - ie if it restarted after error it would be using a last_saved_row > 0
     print(paste("Loop number:", i)) # Adjust loop number based on last saved row - ie if it restarted after error it would be using a last_saved_row > 0
-
+    
     # Clean up any previous connections - this step may not be needed but it seems a clean way to do it
     if (exists("url_html_live")) {
       tryCatch({
@@ -130,27 +137,27 @@ imap(urls, function(x, i) {
       })
       rm(url_html_live)
     }
-
+    
     # Remove previous objects - prob not needed but again, it seems a clean way to do it
     if (exists("image_url")) {rm(image_url)}
-
+    
     # Politely sleep longer after every 30th iteration - so once in a while we pause the scraping for longer- we may not need such a long sleep here?
     if (i %% 30 == 0) {
       message("Sleeping for 10 minutes after every 30 iterations.")
       Sys.sleep(600)
     }
-
+    
     page_data <- get_html_with_retry(x) #run get_html_with_retry() which saves data to a list which we extract below
-
+    
     # Extracted data from page_data
     image_url <- page_data$image_url
-
+    
     print(paste("URL:",x))
     print(paste("Image url:", image_url)) #so we can see how we are tracking
-
+    
     filename <- paste0(folder_path, "/", basename(x), ".png")
     if(!is.na(image_url)){download.file(image_url, destfile = filename, mode = 'wb')}
-
+    
   }, error = function(e) {
     # If any error occurs during the loop then sleep for 10 minutes, and retry
     # message("Error occurred in iteration ", i + last_saved_row, ": ", e$message)
@@ -159,22 +166,10 @@ imap(urls, function(x, i) {
     Sys.sleep(600)
     return(NULL)  # This will retry  the current iteration after the 10-minute sleep
   })
-
+  
 }, .progress = TRUE)
 # ------------------------------------------------------------------------------
 
 image_time_end <- Sys.time()
 image_time_duration <- image_time_end - image_time_start
 image_time_duration
-
-
-
-
-
-
-
-
-
-
-
-
